da2f3bd097e68d87bf5376ff42de25af9b3ce161
easyswap-team <team@easyswap.finance>
2021-05-05 13:17:55 +0300

    Update contract addresses for mainnet_v0.1.0-rc.3

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -2,9 +2,9 @@ import OrderBook from "@sushiswap/settlement/deployments/kovan/OrderBook.json";
 import Settlement from "@sushiswap/settlement/deployments/mainnet/Settlement.json";
 
 // export const UNISWAP_ROUTER = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
-export const SUSHISWAP_ROUTER = "0xC3397a4aeB95529507043BFB914736556ae091A2";
+export const SUSHISWAP_ROUTER = "0x96a1Dee27d4981D98FF9eC95e7f6d45b373118B0";
 export const ROUTER = SUSHISWAP_ROUTER;
-export const MASTER_CHEF = "0xA364f7C9347630DeaDC38149Fc437E04Ee10d00E";
+export const MASTER_CHEF = "0xF6bFF5215A844e5b3eC79B0645ba467BD14416A4";
 export const SUSHI_BAR = "0x6342c257Cb0094761C3AB3Aa7D6d8Ff031c8a123";
 export const SUSHI_ROLL = "0x02728f44db8FAd3347A9CE25FFCf40ffab8b8108";
 export const LP_TOKEN_SCANNER = "0xcF1829b4446bd8bf544597C7206a7BCE020A35a2";
