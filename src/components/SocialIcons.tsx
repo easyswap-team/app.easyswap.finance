399fd42776e0bd9e242ab5075f8ff8da87575005
easyswap-team <team@easyswap.finance>
2021-05-11 17:19:37 +0300

    Add link in social icons

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -13,17 +13,13 @@ import FlexView from "./FlexView";
 
 const SocialIcons = () => {
     const { darkMode } = useContext(GlobalContext);
-    const { background, textLight, white } = useColors();
-    const onPressTwitter = useLinker("https://twitter.com/EasyswapFinance", "", "_blank");
-    const onPressGithub = useLinker("https://github.com/easyswap-team", "", "_blank");
-    const onPressTelegram = useLinker("https://t.me/easyswapFinance", "", "_blank");
-    const onPressM = useLinker("https://medium.com/@EasyswapFinance", "", "_blank");
+    const { white } = useColors();
     return (
         <FlexView style={{ width: "300px", justifyContent: "space-around"}}>
-            <GithubIconLight color={darkMode ? white : '#4373EE'} onPress={onPressGithub} />
-            <TwitterIconLight color={darkMode ? white : '#4373EE'} onPress={onPressTwitter} />
-            <TelegramIconLight color={darkMode ? white : '#4373EE'} onPress={onPressTelegram} />
-            <MIconLight color={darkMode ? white : '#4373EE'} onPress={onPressM} />
+            <a target='_blank' href='https://github.com/easyswap-team'><GithubIconLight color={darkMode ? white : '#4373EE'} /></a>
+            <a target='_blank' href='https://twitter.com/EasyswapFinance'><TwitterIconLight color={darkMode ? white : '#4373EE'} /></a>
+            <a target='_blank' href='https://t.me/easyswapFinance'><TelegramIconLight color={darkMode ? white : '#4373EE'} /></a>
+            <a target='_blank' href='https://medium.com/@EasyswapFinance'><MIconLight color={darkMode ? white : '#4373EE'} /></a>
         </FlexView>
     );
 };
