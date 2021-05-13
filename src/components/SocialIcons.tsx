6637e3ad1fe5aa32e0fbfe076ea03fd8508de153
easyswap-team <team@easyswap.finance>
2021-05-13 18:06:01 +0300

    Add correct twitter url

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -17,7 +17,7 @@ const SocialIcons = () => {
     return (
         <FlexView style={{ width: "300px", justifyContent: "space-around"}}>
             <a target='_blank' href='https://github.com/easyswap-team'><GithubIconLight color={darkMode ? white : '#4373EE'} /></a>
-            <a target='_blank' href='https://twitter.com/EasyswapFinance'><TwitterIconLight color={darkMode ? white : '#4373EE'} /></a>
+            <a target='_blank' href='https://twitter.com/EasyswapF'><TwitterIconLight color={darkMode ? white : '#4373EE'} /></a>
             <a target='_blank' href='https://t.me/easyswapFinance'><TelegramIconLight color={darkMode ? white : '#4373EE'} /></a>
             <a target='_blank' href='https://medium.com/@EasyswapFinance'><MIconLight color={darkMode ? white : '#4373EE'} /></a>
         </FlexView>
